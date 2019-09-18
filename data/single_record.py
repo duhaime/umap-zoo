d6385ee02f369ff18cdc9bda1b373520c6a21ab8
duhaime <douglas.duhaime@gmail.com>
2019-09-18 09:10:39 -0400

    move notebook to script

--- data/single_record.py
+++ data/single_record.py
@@ -1,14 +1,16 @@
 from get_json import get_model_json
-import vertices, umap, json
+import vertices, umap, json, os
+import numpy as np
 
-filename = 'hotdog'
 min_dist = 0.0001
 n_neighbors = 50
+n_vertices = 50000
 
-v = vertices.ObjParser('../data/obj/hotdog.obj').get_n_vertices(50000)
+filename = 'trump-{}'.format(n_vertices)
+v = vertices.ObjParser('../data/obj/trump.obj').get_n_vertices(n_vertices)
 np.save(os.path.join('out', filename + '-3d'), v)
 # project to 2D
 o = umap.UMAP(min_dist=min_dist, n_neighbors=n_neighbors).fit_transform(v)
 np.save(os.path.join('out', filename + '-2d'), o)
-
-get_model_json('hotdog')
+# save the model json for visualizing
+get_model_json('trump')
